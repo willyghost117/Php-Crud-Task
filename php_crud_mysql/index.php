8ae315f0003f4f4bb45a4bf89f8a924f835ca38c
willyghost117 <38368132+willyghost117@users.noreply.github.com>
2023-01-03 16:24:05 -0600

    Primer commmit

---
+++ php_crud_mysql/index.php
@@ -0,0 +1,19 @@
+<?php include ("db.php")?>
+<?php include("includes/header.php")?>
+
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card card-body">
+                    <form action="">
+
+                    </form>
+                </div>
+            </div>
+            
+            <div class="col-md-8">  
+            </div>
+        </div> 
+    </div>
+
+<?php include ("includes/footer.php")?>    
